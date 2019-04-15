9f546396da0d06b21b450cde698478a134ad5893
jameshughes7 <hugjimbo77@gmail.com>
2019-04-15 12:55:57 +0100

    added process.args to take address input from user

--- app.js
+++ app.js
@@ -2,17 +2,23 @@ const request = require('request');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Austin', (error, data) => {
-    if (error) {
-        return console.log(error);
-    }
-    
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+const address  = process.argv[2];
+
+if (!address) {
+    console.log(`Please provide an address`)
+} else {
+    geocode(address, (error, data) => {
         if (error) {
             return console.log(error);
         }
-
-        console.log(data.location);
-        console.log(forecastData);
+        
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error);
+            }
+    
+            console.log(data.location);
+            console.log(forecastData);
+        })
     })
-})
+}
