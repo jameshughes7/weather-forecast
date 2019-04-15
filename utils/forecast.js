72613cce0c93da56c440307a429424e6b3a53a4e
jameshughes7 <hugjimbo77@gmail.com>
2019-04-15 14:00:49 +0100

    refactored using object destructuring and property shorthand

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/0eac6225d3f5590ea4772d6ff76a91f7/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '?units=si';
 
-    request( {url: url, json: true}, (error, response) => {
+    request( { url, json: true}, (error, { body }) => {
         if (error) {
             callback(`Unable to connect to Forecast service`, undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback(`Unable to find location`, undefined)
         } else { 
-            callback(undefined, `${response.body.daily.data[0].summary} It is ${response.body.currently.temperature} out. There is a ${response.body.currently.precipProbability}% of rain.`);
+            callback(undefined, `${body.daily.data[0].summary} It is ${body.currently.temperature} out. There is a ${body.currently.precipProbability}% of rain.`);
         }
     })
 }
