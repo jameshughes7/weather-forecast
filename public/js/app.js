43936b1b4f72e8436abf397010408e46f1b26bc4
jameshughes7 <hugjimbo77@gmail.com>
2019-04-16 12:27:41 +0100

    increased size of input field

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,23 @@
 const weatherForm = document.querySelector('form');
 const searchElement = document.querySelector('input');
+const messageOne = document.querySelector('#messageOne');
+const messageTwo = document.querySelector('#messageTwo');
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const location = searchElement.value;
+    messageOne.textContent = 'Loading ...';
 
     // fetch allows us to get data from a url and do something with it
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error)
+                messageTwo.textContent = data.error;
             } else {
-                console.log(data.address);
-                console.log(data.forecast);
-                console.log(data.location);
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+                
             }
         })
     })
