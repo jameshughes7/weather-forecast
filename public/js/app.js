f4a290ca19cd4ee5ef3c874b55696766c589b938
jameshughes7 <hugjimbo77@gmail.com>
2019-04-16 11:51:13 +0100

    using fetch api to get weather data

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
-console.log(`Client side js file is loaded`);
+// fetch allows us to get data from a url and do something with it
+fetch('http://localhost:3000/weather?address=!').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.address);
+            console.log(data.forecast);
+            console.log(data.location);
+        }
+    })
+})
