50f969d349dae79470d6ee654dbb97e414e696d1
jameshughes7 <hugjimbo77@gmail.com>
2019-04-16 15:02:42 +0100

    more weather data added

--- utils/forecast.js
+++ utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback(`Unable to find location`, undefined)
         } else { 
-            callback(undefined, `${body.daily.data[0].summary} It is ${body.currently.temperature} out. There is a ${body.currently.precipProbability}% of rain.`);
+            callback(undefined, `${body.daily.data[0].summary} It is ${body.currently.temperature} out. There is a ${body.currently.precipProbability}% of rain.The low is ${body.daily.data[0].temperatureLow} and the high is the ${body.daily.data[0].temperatureHigh}`);
         }
     })
 }
