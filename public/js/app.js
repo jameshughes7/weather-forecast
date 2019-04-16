96bc29e9226a3551fe809d60e37f0c37ade260cc
jameshughes7 <hugjimbo77@gmail.com>
2019-04-16 12:06:04 +0100

    form created and fetch api call migrated to when form sumbitted

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,21 @@
-// fetch allows us to get data from a url and do something with it
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.address);
-            console.log(data.forecast);
-            console.log(data.location);
-        }
+const weatherForm = document.querySelector('form');
+const searchElement = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const location = searchElement.value;
+
+    // fetch allows us to get data from a url and do something with it
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.address);
+                console.log(data.forecast);
+                console.log(data.location);
+            }
+        })
     })
 })
